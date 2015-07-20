781c9c6f2a278e8761e10d2b7f2dd686042d4a96
Hanna Hardeman <hanna.hardeman@geodan.nl>
2015-07-20 16:58:10 +0200

    Added GeoQuester

---
+++ src/Geodan.Cloud.Client.GeoQuester/Models/IsOverlapResult.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Geodan.Cloud.Client.GeoQuester.Models
+{
+    public class IsOverlapResult
+    {
+        /// <summary>
+        /// States if the feature intersects with layer shape
+        /// </summary>
+        [JsonProperty(PropertyName = "result")]
+        public bool IsOverlap { get; set; }
+    }
+}
---
+++ src/Geodan.Cloud.Client.GeoQuester/Models/Layer.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geodan.Cloud.Client.GeoQuester.Models
+{
+    public class Layer
+    {
+        /// <summary>
+        /// Name of the account
+        /// </summary>
+        [JsonProperty(PropertyName = "account")]
+        public string Account { get; set; }
+
+        /// <summary>
+        /// Name of the Service
+        /// </summary>
+        [JsonProperty(PropertyName = "service")]
+        public string Service { get; set; }
+
+        /// <summary>
+        /// Name of the layer
+        /// </summary>
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// Id of the layer
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public string LayerId { get; set; }
+
+        /// <summary>
+        /// Expiration date, empty for no expiration
+        /// </summary>
+        [JsonProperty(PropertyName = "expiration")]
+        public string ExpirationDate { get; set; }
+
+        /// <summary>
+        /// Title of the layer
+        /// </summary>
+        [JsonProperty(PropertyName = "title")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Description of the layer
+        /// </summary>
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Content type of the data
+        /// </summary>
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// States if the document will be visible for other accounts
+        /// </summary>
+        [JsonProperty(PropertyName = "public")]
+        public bool IsPublic { get; set; }
+    }
+}
